21aeb9503a30ddeb3b051b0e208c6418fd68c827
Walter Jiron <walteralexeisamadrojiron@gmail.com>
2025-04-05 13:33:42 -0600

    Primer commit

---
+++ src/app.module.ts
@@ -0,0 +1,33 @@
+import { Module } from '@nestjs/common';
+import {ConfigModule} from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
+import { RolesModule } from './roles/roles.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
+    TypeOrmModule.forRoot({
+      type: 'mssql',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT!),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      synchronize: false,
+      autoLoadEntities: true,
+      options: {
+        encrypt: false,
+        trustServerCertificate: true,
+      },
+    }),
+
+    UsersModule,
+
+    RolesModule
+  ],
+})
+export class AppModule {}
