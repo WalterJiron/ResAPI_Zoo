013a6f3b0844879bbc3ed15ea5ff92de1f78cb0b
Walter Jiron <walteralexeisamadrojiron@gmail.com>
2025-04-19 23:26:06 -0600

    Mejorando la seguridad de las variables delicadas

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -16,7 +16,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
       inject: [ConfigService],
       useFactory: async (configService: ConfigService) => ({
         secret: configService.get('JWT_ACCESS_SECRET'),
-        signOptions: { expiresIn: '15m' }, 
+        signOptions: { expiresIn: configService.get('JWT_ACCESS_EXPIRES_IN') }, 
       }),
     }),
   ],
